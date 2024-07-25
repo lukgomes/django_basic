4bdac0469c5fde92575a7ee1b35dd33065f04c64
Lucas F Gomes Santos <lukmck@gmail.com>
2024-07-25 17:17:20 -0300

    atualizando recuros do painel administrativo

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
+class Choiceinline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [Choiceinline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question-text"]
+
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
